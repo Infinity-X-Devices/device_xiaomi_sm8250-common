df0298abc1118c64e15eceeb8e6a9838ca57c91b
aswin7469 <aswinas@pixysos.com>
2024-02-19 17:47:22 +0545

    sm8250-common: Update Audio HIDL to 7.0
    * 7.1 crashes in 14 , 7.0 hal is enough to get spatial audio work

--- kona.mk
+++ kona.mk
@@ -99,8 +99,8 @@ PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 # Audio
 PRODUCT_PACKAGES += \
     audioadsprpcd \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.bluetooth.audio@2.1-impl \
